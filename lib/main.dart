599bdb207d645ca4e1afa909078ddf5478921262
vivekkuvadiya <vivekpatel9054@gmail.com>
2021-03-31 21:14:03 +0530

    button & click

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.cyan,
+    ),
+    title: "Statfull Widgets",
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var dataChanged = 0;
+
+  dataChange() {
+    setState(() {
+      dataChanged += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sample App",
-      theme:
-          ThemeData(accentColor: Colors.cyan, primarySwatch: Colors.lightBlue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Sample App"),
-        ),
-        body: Center(
-          child: Text(
-            "This is new Flutter App Project",
-            style: TextStyle(
-                color: Colors.blueGrey,
-                fontWeight: FontWeight.bold,
-                fontSize: 30.25),
-          ),
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("StatFull Widgets"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "$dataChanged",
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: Colors.red,
+                splashColor: Colors.black,
+                textColor: Colors.white,
+                onPressed: dataChange,
+                child: Text(
+                  "Button",
+                  style: TextStyle(fontSize: 20.0),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
