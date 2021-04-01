2ea54633861d8bb8f668886f56490eadfea0aa9a
vivekkuvadiya <vivekpatel9054@gmail.com>
2021-04-01 21:07:26 +0530

    edittext and get Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    theme: ThemeData(
+      primarySwatch: Colors.cyan,
+    ),
+    title: "Input Filed",
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Sample App",
-      theme:
-          ThemeData(accentColor: Colors.cyan, primarySwatch: Colors.lightBlue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Sample App"),
-        ),
-        body: Center(
-          child: Text(
-            "This is new Flutter App Project",
-            style: TextStyle(
-                color: Colors.blueGrey,
-                fontWeight: FontWeight.bold,
-                fontSize: 30.25),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Input Filed"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(name != null ? "$name" : "-"),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              maxLength: 10,
+              decoration: InputDecoration(hintText: "Username",
+              prefixIcon: Icon(Icons.verified_user)),
+              style: TextStyle(fontSize: 25.0, color: Colors.deepPurple),
+              onChanged: (text) {
+                if (text != "") {
+                  name = text;
+                }else{
+                  name = null;
+                }
+              },
+            ),
           ),
-        ),
+          MaterialButton(
+            onPressed: () {
+              setText();
+            },
+            color: Colors.pink,
+            child: Text(
+              "Press",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  setText() {
+    setState(() {});
   }
 }
